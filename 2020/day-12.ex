67f58ecdef93d865cb39ee0a6073bd966235846e
Felix Alonso <Xleif5@gmail.com>
2020-12-12 11:29:04 -0800

    remove symbols from day12

--- 2020/day-12.ex
+++ 2020/day-12.ex
@@ -1,41 +1,27 @@
 defmodule Day12 do
 
-  def directions do 
-    %{
-      "F" => :forward,
-      "L" => :left,
-      "R" => :right,
-      "N" => :north,
-      "E" => :east,
-      "S" => :south,
-      "W" => :west,
-    }
-  end
-
-  def rotations, do: [:north, :east, :south, :west]
+  def rotations, do: ["N", "E", "S", "W"]
 
   def rotate(relative, dir, places) do
     cur = Enum.find_index(rotations(), &(relative == &1))
     places = div(places, 90)
     case dir do
-      :right -> Enum.at(rotations(), rem(cur + places, 4))
-      :left  -> Enum.at(rotations(), rem(cur - places, 4))
+      "R" -> Enum.at(rotations(), rem(cur + places, 4))
+      "L"  -> Enum.at(rotations(), rem(cur - places, 4))
     end
   end
 
-  def parse_direction(<<dir :: binary-size(1)>> <> dist) do
-    {Map.get(directions(), dir), String.to_integer(dist)}
-  end
+  def parse(<<dir :: binary-size(1)>> <> dist), do: {dir, String.to_integer(dist)}
   
   def load do
     File.read!("day-12.input")
     |> String.split("\n", trim: true)
-    |> Enum.map(&parse_direction/1)
+    |> Enum.map(&parse/1)
   end
 end
 
 defmodule Day12.Ship do
-  defstruct [dir: :east, x: 0, y: 0]
+  defstruct [dir: "E", x: 0, y: 0]
 end
 
 defmodule Day12.Waypoint do
@@ -43,12 +29,12 @@ defmodule Day12.Waypoint do
 
   defstruct [x: 10, y: 1]
 
-  def move(w=%{y: y}, :north, dist), do: %{w | y: y + dist}
-  def move(w=%{y: y}, :south, dist), do: %{w | y: y - dist}
-  def move(w=%{x: x}, :east,  dist), do: %{w | x: x + dist}
-  def move(w=%{x: x}, :west,  dist), do: %{w | x: x - dist}
+  def move(w=%{y: y}, "N", dist), do: %{w | y: y + dist}
+  def move(w=%{y: y}, "S", dist), do: %{w | y: y - dist}
+  def move(w=%{x: x}, "E", dist), do: %{w | x: x + dist}
+  def move(w=%{x: x}, "W", dist), do: %{w | x: x - dist}
 
-  def rotate(w=%{x: x, y: y}, relative, :right, rotations) do
+  def rotate(w=%{x: x, y: y}, "R", rotations) do
     case rotations |> div(90) |> rem(4) do
       0 -> w
       1 -> %{w | x: y, y: -x}
@@ -57,7 +43,7 @@ defmodule Day12.Waypoint do
     end
   end
 
-  def rotate(w=%{x: x, y: y}, relative, :left, rotations) do
+  def rotate(w=%{x: x, y: y}, "L", rotations) do
     case rotations |> div(90) |> rem(4) do
       0 -> w
       1 -> %{w | x: -y, y: x}
@@ -77,12 +63,12 @@ defmodule Day12.Part1 do
     |> (fn %{x: x, y: y} -> abs(x) + abs(y) end).()
   end
 
-  def move({:forward, dist}, state=%{dir: dir}), do: move({dir, dist}, state)    
+  def move({"F", dist}, state=%{dir: dir}), do: move({dir, dist}, state)    
 
-  def move({:north, dist}, state=%{y: y}), do: %{state | y: y + dist}    
-  def move({:south, dist}, state=%{y: y}), do: %{state | y: y - dist}    
-  def move({:east, dist}, state=%{x: x}),  do: %{state | x: x + dist}    
-  def move({:west, dist}, state=%{x: x}),  do: %{state | x: x - dist}    
+  def move({"N", dist}, state=%{y: y}), do: %{state | y: y + dist}    
+  def move({"S", dist}, state=%{y: y}), do: %{state | y: y - dist}    
+  def move({"E", dist}, state=%{x: x}), do: %{state | x: x + dist}    
+  def move({"W", dist}, state=%{x: x}), do: %{state | x: x - dist}    
 
   def move({dir, dist}, s=%{dir: relative}), do: %{s | dir: rotate(relative, dir, dist)}
 end
@@ -98,16 +84,14 @@ defmodule Day12.Part2 do
     |> (fn {%{x: x, y: y}, _} -> abs(x) + abs(y) end).()
   end
 
-  def move({:north, dist}, {s, w}), do: {s, Waypoint.move(w, :north, dist)}
-  def move({:south, dist}, {s, w}), do: {s, Waypoint.move(w, :south, dist)}
-  def move({:east,  dist}, {s, w}), do: {s, Waypoint.move(w, :east, dist)}
-  def move({:west,  dist}, {s, w}), do: {s, Waypoint.move(w, :west, dist)}
+  def move({"N", dist}, {s, w}), do: {s, Waypoint.move(w, "N", dist)}
+  def move({"S", dist}, {s, w}), do: {s, Waypoint.move(w, "S", dist)}
+  def move({"E", dist}, {s, w}), do: {s, Waypoint.move(w, "E", dist)}
+  def move({"W", dist}, {s, w}), do: {s, Waypoint.move(w, "W", dist)}
 
-  def move({:forward,  dist}, {s=%{x: sx, y: sy}, w=%{x: wx, y: wy}}) do 
+  def move({"F",  dist}, {s=%{x: sx, y: sy}, w=%{x: wx, y: wy}}) do 
     {%{s | x: sx + dist * wx, y: sy + dist * wy}, w}
   end
 
-  def move({dir, rotations}, {s=%{dir: relative}, w}) do 
-    {s, Waypoint.rotate(w, relative, dir, rotations)}
-  end
+  def move({dir, rotations}, {s, w}), do: {s, Waypoint.rotate(w, dir, rotations)}
 end
